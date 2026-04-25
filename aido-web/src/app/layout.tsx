e421b5a40b753edfb58e839abff261eb7e0d7b24
ahmadzz <royanuahmad313@gmail.com>
2026-04-25 13:07:54 +0700

    fix: add suppressHydrationWarning attribute to RootLayout

--- aido-web/src/app/layout.tsx
+++ aido-web/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default async function RootLayout({
 
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
